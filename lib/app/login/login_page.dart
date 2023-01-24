c137b9ee345835af4b96649d37cb9de813ad277f
Julia Czaplejewicz <czaplejewiczjulia@gmail.com>
2023-01-24 18:33:44 +0100

    Create login screen

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,16 +1,47 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('You are not logged in'),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Try to sign in'),
+              const SizedBox(height: 15),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: 'E-mail:'),
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(hintText: 'Password:'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  try { await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text);} catch (error) {}
+                 
+                },
+                child: const Text('Sign in'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class RootPage extends StatelessWidget {
         builder: (context, snapshot) {
           final user = snapshot.data;
           if (user == null) {
-            return const LoginPage();
+            return  LoginPage();
           }
           return HomePage(user: user);
         });
