00806c0be1d7f831fd0fc29d628965b7ccc09d77
Julia Czaplejewicz <czaplejewiczjulia@gmail.com>
2023-07-31 22:34:14 +0200

    Fix the bug

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
